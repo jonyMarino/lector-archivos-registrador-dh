026ab3e64908e398c76fb6485bb5637ae62fd5af
jonymarino <jonymarino@d76bd577-2da0-11c1-3b5d-fd3389f7f60f>
2011-08-12 16:56:04 +0000

    traductor de archivos a excel y access

---
+++ trunk/src/traductorarchivosdh/TraductorArchivosDHApp.java
@@ -0,0 +1,44 @@
+/*
+ * TraductorArchivosDHApp.java
+ */
+
+package traductorarchivosdh;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class TraductorArchivosDHApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new TraductorArchivosDHView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of TraductorArchivosDHApp
+     */
+    public static TraductorArchivosDHApp getApplication() {
+        return Application.getInstance(TraductorArchivosDHApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(TraductorArchivosDHApp.class, args);
+    }
+}
